49f988cef6f59ffbae8ba49cd0bd544850e3bc2f
noob <setking929@gmail.com>
2024-03-27 15:19:26 +0900

    회원가입, 회원목록 조회 개발

--- src/main/java/hello/hellospring/repository/MemoryMemeberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemeberRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-@Repository
+
 public class MemoryMemeberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
